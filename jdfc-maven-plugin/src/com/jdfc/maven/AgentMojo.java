0b70783586af5e45ce4a4d49b22bf35785863669
Matthias Bloch <m4bl94@gmail.com>
2023-01-31 14:42:16 +0100

    Add agent jar to maven pluign, try to retrieve agent jar from maven plugin jar in agent mojo

--- jdfc-maven-plugin/src/com/jdfc/maven/AgentMojo.java
+++ jdfc-maven-plugin/src/com/jdfc/maven/AgentMojo.java
@@ -1,16 +1,16 @@
 package com.jdfc.maven;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
-import java.io.File;
-import java.util.Map;
-import java.util.Properties;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 import static java.lang.String.format;
 
@@ -21,30 +21,61 @@ public class AgentMojo extends AbstractJdfcMojo {
      * Map of plugin artifacts.
      */
     @Parameter(property = "plugin.artifactMap", required = true, readonly = true)
-    Map<String, Artifact> pluginArtifactMap;
+    private Map<String, Artifact> pluginArtifactMap;
 
-    static final String AGENT_ARTIFACT_NAME = "com.jdfc:com.jdfc.agent";
+    static final String AGENT_FILE_NAME = "target/com.jdfc.agent-1.0-SNAPSHOT-runtime.jar";
 
     @Override
-    protected void executeMojo() {
-        String argLine = "argLine";
-        Properties projectProperties = getProject().getProperties();
-        String oldValue = projectProperties.getProperty(argLine);
-        // create command line arguments for agent
-        final String targetDir = getProject().getBuild().getOutputDirectory();
-        final String agent = format("-javaagent:%s", getAgentJarFile());
-        String newValue;
-        if (oldValue == null) {
-            newValue = String.format("%s=%s", agent, targetDir);
-        } else {
-            newValue = String.format("%s %s=%s", oldValue, agent, targetDir);
+    protected void executeMojo()  {
+        final String argLine = "argLine";
+        final Artifact pluginArtifact = pluginArtifactMap.get("com.jdfc:jdfc-maven-plugin");
+        final File pluginJarFile = pluginArtifact.getFile();
+        try {
+            final File agentJarFile = extractAgentJarToTargetDir(pluginJarFile, AGENT_FILE_NAME, getProject().getBasedir());
+            if (agentJarFile != null) {
+                final Properties projectProperties = getProject().getProperties();
+                final String oldValue = projectProperties.getProperty(argLine);
+                // create command line arguments for agent
+                final String agent = format("-javaagent:%s", agentJarFile);
+                final File classesDir = new File(getProject().getBuild().getOutputDirectory());
+                String newValue;
+                if (oldValue == null) {
+                    newValue = String.format("%s=%s", agent, classesDir);
+                } else {
+                    newValue = String.format("%s %s=%s", oldValue, agent, classesDir);
+                }
+                projectProperties.setProperty(argLine, newValue);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        projectProperties.setProperty(argLine, newValue);
     }
 
-    File getAgentJarFile() {
-        final Artifact jdfcAgentArtifact = pluginArtifactMap
-                .get(AGENT_ARTIFACT_NAME);
-        return jdfcAgentArtifact.getFile();
+    public File extractAgentJarToTargetDir(File jarFile, String fileName, File destDirectory) throws Exception {
+        JarInputStream jarInputStream = new JarInputStream(Files.newInputStream(jarFile.toPath()));
+        JarEntry jarEntry;
+        File file = null;
+        while ((jarEntry = jarInputStream.getNextJarEntry()) != null) {
+            if (jarEntry.getName().equals(fileName)) {
+                String[] fileNames = jarEntry.getName().split("/");
+                for(int i = 0; i < fileNames.length - 1; i++) {
+                    File dir = new File(fileNames[i]);
+                    if (!dir.exists()) {
+                        dir.mkdirs();
+                    }
+                }
+                file = new File(destDirectory, fileName);
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = jarInputStream.read(buffer)) > 0) {
+                    fileOutputStream.write(buffer, 0, length);
+                }
+                fileOutputStream.close();
+                break;
+            }
+        }
+        jarInputStream.close();
+        return file;
     }
 }
