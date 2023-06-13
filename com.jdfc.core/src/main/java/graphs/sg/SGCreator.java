5ab56ad7d5055f7cc4ecc3c1f1b014941741c6d4
Matthias Bloch <m4bl94@gmail.com>
2023-06-13 20:01:54 +0200

    Add SGCreator (empty)

---
+++ com.jdfc.core/src/main/java/graphs/sg/SGCreator.java
@@ -0,0 +1,22 @@
+package graphs.sg;
+
+public class SGCreator {
+
+//    public static SG createSGsForClass(ClassExecutionData cData) {
+//        for(MethodData mData : cData.getMethods().values()) {
+//            NavigableMap<Integer, SGNode> nodes = Maps.newTreeMap();
+//            Multimap<Integer, Integer> edges = ArrayListMultimap.create();
+//
+//            CFG cfg = mData.getCfg();
+//            for(Map.Entry<Double, CFGNode> nodeEntry : cfg.getNodes().entrySet()) {
+//                Double idx = nodeEntry.getKey();
+//                CFGNode node = nodeEntry.getValue();
+//                if (nodeEntry.getValue() instanceof CFGEntryNode) {
+//                    // Todo: change cfg node keys to integer
+//                    // Todo: make entryNode have index 0
+//                    SGNode n = new SGEntryNode();
+//                }
+//            }
+//        }
+//    }
+}
