ad5f41fee025ddc58ddd1fe7a7239490a37d74ee
Matthias Bloch <m4bl94@gmail.com>
2023-06-19 11:29:27 +0200

    Add getPVarArgs to CFGEntryNode

--- com.jdfc.core/src/main/java/graphs/cfg/nodes/CFGEntryNode.java
+++ com.jdfc.core/src/main/java/graphs/cfg/nodes/CFGEntryNode.java
@@ -3,6 +3,8 @@ package graphs.cfg.nodes;
 import data.ProgramVariable;
 import utils.JDFCUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class CFGEntryNode extends CFGNode {
@@ -15,6 +17,26 @@ public class CFGEntryNode extends CFGNode {
     public String toString() {
         return String.format(
                 "CFGEntryNode: %d %s (%d preds, %d succs) | definitions %s | uses %s",
-                this.getInsnIndex(), JDFCUtils.getOpcode(this.getOpcode()), this.getPred().size(), this.getSucc().size(), this.getDefinitions(), this.getUses());
+                this.getInsnIndex(), JDFCUtils.getOpcode(this.getOpcode()), this.getPred().size(),
+                this.getSucc().size(), this.getDefinitions(), this.getUses());
+    }
+
+    // --- Getters, Setters --------------------------------------------------------------------------------------------
+
+    // --- Helper Methods ----------------------------------------------------------------------------------------------
+
+    public Map<Integer, ProgramVariable> getPVarArgs() {
+        Map<Integer, ProgramVariable> pVarArgs = new HashMap<>();
+
+        // TODO: keep an eye on this
+        int counter = 0;
+        for(ProgramVariable d : this.getDefinitions()) {
+            if (!d.getName().equals("this")) {
+                pVarArgs.put(counter, d);
+                counter++;
+            }
+        }
+
+        return pVarArgs;
     }
 }
