175e2453ce4a23cf4e241ae141725fe81ea2ce43
Matthias Bloch <m4bl94@gmail.com>
2023-06-23 07:59:21 +0200

    Comment out TabulationAlgorithm

--- com.jdfc.core/src/main/java/algos/TabulationAlgorithm.java
+++ com.jdfc.core/src/main/java/algos/TabulationAlgorithm.java
@@ -2,12 +2,9 @@ package algos;
 
 import graphs.esg.ESG;
 import graphs.esg.ESGEdge;
-import graphs.esg.nodes.ESGCallNode;
-import graphs.esg.nodes.ESGExitNode;
-import graphs.esg.nodes.ESGNode;
-import graphs.sg.ZeroVariable;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class TabulationAlgorithm {
 
@@ -20,50 +17,50 @@ public class TabulationAlgorithm {
     private LinkedList<ESGEdge> workList;
 
     public TabulationAlgorithm(ESG graph) {
-        this.graph = graph;
-        this.pathEdgeSet = new HashSet<>();
-        this.summaryEdgeSet = new HashSet<>();
-        this.workList = new LinkedList<>();
-
-        ESGNode mainNode = new ESGNode(graph.getSg().getEntryNode(), new ZeroVariable(), true);
-        pathEdgeSet.add(new ESGEdge(0, mainNode, 0, mainNode));
-        this.workList.add(new ESGEdge(0, mainNode, 0, mainNode));
+//        this.graph = graph;
+//        this.pathEdgeSet = new HashSet<>();
+//        this.summaryEdgeSet = new HashSet<>();
+//        this.workList = new LinkedList<>();
+//
+//        ESGNode mainNode = new ESGNode(graph.getSg().getEntryNode(), new ZeroVariable(), true);
+//        pathEdgeSet.add(new ESGEdge(0, mainNode, 0, mainNode));
+//        this.workList.add(new ESGEdge(0, mainNode, 0, mainNode));
     }
 
     public void execute() {
-        while(!this.workList.isEmpty()) {
-            ESGEdge e = workList.pop();
-
-            if (e.getTarget() instanceof ESGCallNode) {
-                // entryNodes = find entry nodes by names from calledProc(e.getTarget)
-                // for (entryNode : entryNodes) {
-                //     if (<e.getTarget(), var> -> <entry(calledProc(n)), var> exists) {
-                //         propagate(new Edge(<entry(calledProc(n)), var>, <entry(calledProc(n)), var>)
-                //     }
-                // }
-                // returnSites = find corresponding return site nodes by return(e.getTarget) mapping
-                // for (returnSiteNode : returnSites) {
-                //     propagate (new Edge(e.getSource(), returnSiteNode)
-                // }
-            } else if (e.getTarget() instanceof ESGExitNode) {
-                // for(CallNode c : callers(p)) { // all call nodes that call procedure p
-                //     get possiblyNotRedefinedVarsDuringProcedure
-                //     for(var : possiblyNotRedefinedVarsDuringProcedure)
-                //         if (<c, var> -> <return(c), var> not in summaryEdgeSet) {
-                //             insert summary edge
-                //             for(var : <entry(procOf(c)), var> -> <c, var> in PathEdge {
-                //                 propagate(new Edge(<entry(procOf(c)), var>, <returnSite(c), var>))
-                //             }
-                //         }
-                //     }
-            } else {
-                // for (node : succ(e.getTarget()) {
-                //     if (edge target -> succ exists) {
-                //         propagate(new Edge(e.getSource(), node)
-                //     }
-                // }
-            }
-        }
+//        while(!this.workList.isEmpty()) {
+//            ESGEdge e = workList.pop();
+//
+//            if (e.getTarget() instanceof ESGCallNode) {
+//                // entryNodes = find entry nodes by names from calledProc(e.getTarget)
+//                // for (entryNode : entryNodes) {
+//                //     if (<e.getTarget(), var> -> <entry(calledProc(n)), var> exists) {
+//                //         propagate(new Edge(<entry(calledProc(n)), var>, <entry(calledProc(n)), var>)
+//                //     }
+//                // }
+//                // returnSites = find corresponding return site nodes by return(e.getTarget) mapping
+//                // for (returnSiteNode : returnSites) {
+//                //     propagate (new Edge(e.getSource(), returnSiteNode)
+//                // }
+//            } else if (e.getTarget() instanceof ESGExitNode) {
+//                // for(CallNode c : callers(p)) { // all call nodes that call procedure p
+//                //     get possiblyNotRedefinedVarsDuringProcedure
+//                //     for(var : possiblyNotRedefinedVarsDuringProcedure)
+//                //         if (<c, var> -> <return(c), var> not in summaryEdgeSet) {
+//                //             insert summary edge
+//                //             for(var : <entry(procOf(c)), var> -> <c, var> in PathEdge {
+//                //                 propagate(new Edge(<entry(procOf(c)), var>, <returnSite(c), var>))
+//                //             }
+//                //         }
+//                //     }
+//            } else {
+//                // for (node : succ(e.getTarget()) {
+//                //     if (edge target -> succ exists) {
+//                //         propagate(new Edge(e.getSource(), node)
+//                //     }
+//                // }
+//            }
+//        }
     }
 
 }
