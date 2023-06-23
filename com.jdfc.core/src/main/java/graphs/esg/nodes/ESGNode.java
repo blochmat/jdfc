ec8bf577376d95b6d637c1f30a8d61f283b4f56f
Matthias Bloch <m4bl94@gmail.com>
2023-06-23 08:00:17 +0200

    Add basic ESGNode

--- com.jdfc.core/src/main/java/graphs/esg/nodes/ESGNode.java
+++ com.jdfc.core/src/main/java/graphs/esg/nodes/ESGNode.java
@@ -1,21 +1,31 @@
 package graphs.esg.nodes;
 
-import graphs.sg.InterVariable;
-import graphs.sg.nodes.SGNode;
+import com.google.common.collect.Sets;
+import data.DomainVariable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ESGNode {
 
-    private SGNode node;
-
-    private InterVariable var;
-
+    private DomainVariable var;
     private boolean isPossiblyNotRedefined;
+    Set<ESGNode> pred;
+    Set<ESGNode> succ;
 
-    // if true create edge to succ(node)
+    public ESGNode(DomainVariable var) {
+        this.var = var;
+        this.isPossiblyNotRedefined = true;
+        this.pred = Sets.newLinkedHashSet();
+        this.succ = Sets.newLinkedHashSet();
+    }
+
+    public boolean isZero() {
+        return var instanceof DomainVariable.ZeroVariable;
+    }
 }
