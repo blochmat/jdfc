d951f53c7b85dd5209bbfaffeb53a7f9255bb752
Matthias Bloch <m4bl94@gmail.com>
2023-06-23 08:01:01 +0200

    Add basic ESGCreator (WIP)

--- com.jdfc.core/src/main/java/graphs/esg/ESGCreator.java
+++ com.jdfc.core/src/main/java/graphs/esg/ESGCreator.java
@@ -1,20 +1,31 @@
 package graphs.esg;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 import data.ClassExecutionData;
 import data.DomainVariable;
 import data.MethodData;
+import data.ProgramVariable;
+import graphs.esg.nodes.ESGNode;
 import graphs.sg.SG;
 import graphs.sg.nodes.SGNode;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class ESGCreator {
 
+    private static int ESG_NODE_INDEX;
+
     public static void createESGsForClass(ClassExecutionData cData) {
         for(MethodData mData : cData.getMethods().values()) {
+            ESG_NODE_INDEX = 0;
             mData.setEsg(ESGCreator.createESGForMethod(cData, mData));
             mData.getSg().calculateReachingDefinitions();
             mData.calculateInterDefUsePairs();
@@ -25,6 +36,22 @@ public class ESGCreator {
         SG sg = mData.getSg();
         Set<DomainVariable> domain = mData.getSg().getDomain();
         Map<DomainVariable, DomainVariable> domainVarMap = mData.getSg().getDomainVarMap();
+        Map<Integer, ESGNode> nodes = Maps.newTreeMap();
+        Multimap<Integer, Integer> edges = ArrayListMultimap.create();
+
+        ESGNode zero = new ESGNode(new DomainVariable.ZeroVariable());
+        nodes.put(ESG_NODE_INDEX, zero);
+
+        for(SGNode sgNode : mData.getSg().getNodes().values()) {
+            Set<ProgramVariable> definitions = sgNode.getDefinitions();
+
+        }
+
+        Set<DomainVariable> initialDVars = domain.stream()
+                .filter(var ->
+                        Objects.equals(var.getMethod(), mData.buildInternalMethodName())).collect(Collectors.toSet());
+
+
 
         for(SGNode node : sg.getNodes().values()) {
             // TODO: For every node create as many nodes as domain vars are present.
@@ -37,4 +64,14 @@ public class ESGCreator {
 
         return null;
     }
+
+    public static Set<ESGNode> createNodes(Set<DomainVariable> dVarSet) {
+        Set<ESGNode> nodes = Sets.newLinkedHashSet();
+        for(DomainVariable dVar : dVarSet) {
+            nodes.add(new ESGNode(dVar));
+        }
+        return nodes;
+    }
+
+
 }
