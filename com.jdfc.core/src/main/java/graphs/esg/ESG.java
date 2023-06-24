bcc465f7ec7212921584ebfb03326d508b156235
Matthias Bloch <m4bl94@gmail.com>
2023-06-24 15:39:42 +0200

    Finish ESG creation

--- com.jdfc.core/src/main/java/graphs/esg/ESG.java
+++ com.jdfc.core/src/main/java/graphs/esg/ESG.java
@@ -1,14 +1,17 @@
 package graphs.esg;
 
+import com.google.common.collect.Multimap;
 import graphs.esg.nodes.ESGNode;
-import graphs.sg.SG;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.NavigableMap;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ESG {
-
-    private SG sg;
-    private NavigableMap<Integer, ESGNode> nodes;
+    private NavigableMap<Integer, NavigableMap<Integer, ESGNode>> nodes;
+    private Multimap<Integer, ESGEdge> edges;
 }
--- com.jdfc.core/src/main/java/graphs/esg/ESGCreator.java
+++ com.jdfc.core/src/main/java/graphs/esg/ESGCreator.java
@@ -9,12 +9,14 @@ import data.MethodData;
 import graphs.esg.nodes.ESGNode;
 import graphs.sg.SG;
 import graphs.sg.nodes.SGCallNode;
+import graphs.sg.nodes.SGExitNode;
 import graphs.sg.nodes.SGNode;
 import lombok.extern.slf4j.Slf4j;
 import utils.JDFCUtils;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.Set;
 
 @Slf4j
@@ -24,15 +26,16 @@ public class ESGCreator {
     public static void createESGsForClass(ClassExecutionData cData) {
         for(MethodData mData : cData.getMethods().values()) {
             mData.setEsg(ESGCreator.createESGForMethod(cData, mData));
-            mData.getSg().calculateReachingDefinitions();
-            mData.calculateInterDefUsePairs();
+//            mData.getSg().calculateReachingDefinitions();
+//            mData.calculateInterDefUsePairs();
         }
     }
 
     public static ESG createESGForMethod(ClassExecutionData cData, MethodData mData) {
         SG sg = mData.getSg();
 
-        Map<Integer, Map<Integer, ESGNode>> esgNodes = Maps.newTreeMap();
+        //--- CREATE NODES ---------------------------------------------------------------------------------------------
+        NavigableMap<Integer, NavigableMap<Integer, ESGNode>> esgNodes = Maps.newTreeMap();
         esgNodes.computeIfAbsent(Integer.MIN_VALUE, k -> Maps.newTreeMap());
         esgNodes.get(Integer.MIN_VALUE).put(-1, new ESGNode.ESGZeroNode(Integer.MIN_VALUE));
 
@@ -47,6 +50,7 @@ public class ESGCreator {
             }
         }
 
+        // --- DEBUG NODES ---------------------------------------------------------------------------------------------
         if(log.isDebugEnabled()) {
             StringBuilder sb = new StringBuilder();
             sb.append(cData.getRelativePath()).append(" ");
@@ -66,26 +70,26 @@ public class ESGCreator {
             JDFCUtils.logThis(sb.toString(), "exploded_nodes");
         }
 
+
+        //--- CREATE EDGES ---------------------------------------------------------------------------------------------
         Multimap<Integer, ESGEdge> esgEdges = ArrayListMultimap.create();
+
         // iterate over sg
         for(SGNode sgNode : sg.getNodes().values()) {
+            Map<Integer, ESGNode> esgSourceNodeMap = esgNodes.get(sgNode.getIndex());
             Collection<Integer> sgnTargetIdxList = sg.getEdges().get(sgNode.getIndex());
 
             // iterate over domain vars
             for(Map.Entry<Integer, ESGNode> esgNodeEntry : esgNodes.get(sgNode.getIndex()).entrySet()) {
                 DomainVariable domainVariable = esgNodeEntry.getValue().getDVar();
-
                 if(sgNode.getIndex() == 0) {
                     // special case for very first node of the sg
                     esgEdges.put(Integer.MIN_VALUE, new ESGEdge(
                             Integer.MIN_VALUE,
-                            -1,
                             0,
+                            -1,
                             domainVariable.getIndex()));
                 }
-
-                // red
-                Map<Integer, ESGNode> esgSourceNodeMap = esgNodes.get(sgNode.getIndex());
 
                 if (sgNode instanceof SGCallNode) {
                     SGCallNode sgCallNode = (SGCallNode) sgNode;
@@ -97,8 +101,8 @@ public class ESGCreator {
                                 // special case zeroVar
                                 esgEdges.put(sgCallNode.getIndex(), new ESGEdge(
                                         sgCallNode.getIndex(),
-                                        sourceDVarIdx,
                                         sgnTargetIdx,
+                                        sourceDVarIdx,
                                         sourceDVarIdx));
                             } else {
                                 Integer targetDVarIdx = sgCallNode.getDVarMap().get(sourceDVarIdx);
@@ -106,38 +110,39 @@ public class ESGCreator {
                                     // green
                                     esgEdges.put(sgCallNode.getIndex(), new ESGEdge(
                                             sgCallNode.getIndex(),
-                                            sourceDVarIdx,
                                             sgnTargetIdx,
+                                            sourceDVarIdx,
                                             targetDVarIdx));
                                 }
                             }
                         }
                     }
-//                } else if (sgNode instanceof SGExitNode) {
-//                    // Iterate over all domain vars in source
-//                    for(Integer sourceDVarIdx : esgSourceNodeMap.keySet()) {
-//                        // for all target sg nodes
-//                        for(Integer sgnTargetIdx: sgnTargetIdxList) {
-//                            if(sourceDVarIdx == -1) {
-//                                // special case zeroVar
-//                                esgEdges.put(sgNode.getIndex(), new ESGEdge(
-//                                        sgNode.getIndex(),
-//                                        sourceDVarIdx,
-//                                        sgnTargetIdx,
-//                                        sourceDVarIdx));
-//                            } else {
-//                                Integer targetDVarIdx = domainVarMap.get(sgNode.getIndex()).inverse().get(sourceDVarIdx);
-//                                if(targetDVarIdx != null) {
-//                                    // green
-//                                    esgEdges.put(sgNode.getIndex(), new ESGEdge(
-//                                            sgNode.getIndex(),
-//                                            sourceDVarIdx,
-//                                            sgnTargetIdx,
-//                                            targetDVarIdx));
-//                                }
-//                            }
-//                        }
-//                    }
+                } else if (sgNode instanceof SGExitNode) {
+                    SGExitNode sgExitNode = (SGExitNode) sgNode;
+                    // Iterate over all domain vars in source
+                    for(Integer sourceDVarIdx : esgSourceNodeMap.keySet()) {
+                        // for all target sg nodes
+                        for(Integer sgnTargetIdx: sgnTargetIdxList) {
+                            if(sourceDVarIdx == -1) {
+                                // special case zeroVar
+                                esgEdges.put(sgNode.getIndex(), new ESGEdge(
+                                        sgNode.getIndex(),
+                                        sgnTargetIdx,
+                                        sourceDVarIdx,
+                                        sourceDVarIdx));
+                            } else {
+                                Integer targetDVarIdx = sgExitNode.getDVarMap().get(sourceDVarIdx);
+                                if(targetDVarIdx != null) {
+                                    // green
+                                    esgEdges.put(sgNode.getIndex(), new ESGEdge(
+                                            sgNode.getIndex(),
+                                            sgnTargetIdx,
+                                            sourceDVarIdx,
+                                            targetDVarIdx));
+                                }
+                            }
+                        }
+                    }
                 } else {
                     // Iterate over all domain vars in source
                     for(Integer sourceDVarIdx : esgSourceNodeMap.keySet()) {
@@ -146,8 +151,8 @@ public class ESGCreator {
                             // green
                             esgEdges.put(sgNode.getIndex(), new ESGEdge(
                                     sgNode.getIndex(),
-                                    sourceDVarIdx,
                                     sgnTargetIdx,
+                                    sourceDVarIdx,
                                     sourceDVarIdx));
                         }
                     }
@@ -155,6 +160,7 @@ public class ESGCreator {
             }
         }
 
+        //--- DEGUG EDGES ----------------------------------------------------------------------------------------------
         if(log.isDebugEnabled()) {
             StringBuilder sb = new StringBuilder();
             sb.append(cData.getRelativePath()).append(" ");
@@ -162,6 +168,8 @@ public class ESGCreator {
             sb.append(JDFCUtils.prettyPrintMultimap(esgEdges));
             JDFCUtils.logThis(sb.toString(), "exploded_edges");
         }
-        return null;
+
+        //--- CREATE ESG -----------------------------------------------------------------------------------------------
+        return new ESG(esgNodes, esgEdges);
     }
 }
