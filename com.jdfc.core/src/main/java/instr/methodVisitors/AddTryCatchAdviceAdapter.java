78db18ff8c59c2b10ec11a714e6a4e88b8882e4c
Matthias Bloch <m4bl94@gmail.com>
2023-09-05 16:00:30 +0200

    Fix AddTryCatchAdviceAdapter for empty methods

--- com.jdfc.core/src/main/java/instr/methodVisitors/AddTryCatchAdviceAdapter.java
+++ com.jdfc.core/src/main/java/instr/methodVisitors/AddTryCatchAdviceAdapter.java
@@ -10,30 +10,35 @@ public class AddTryCatchAdviceAdapter extends AdviceAdapter {
     private final Label startTryLabel = new Label();
     private final Label endTryLabel = new Label();
     private final Label startCatchLabel = new Label();
+    private boolean isEmpty = true;
 
     public AddTryCatchAdviceAdapter(int api, MethodVisitor mv, int access, String name, String desc) {
         super(api, mv, access, name, desc);
     }
 
     @Override
-    protected void onMethodEnter() {
+    public void visitCode() {
         mv.visitLabel(startTryLabel);
+        super.visitCode();
+        isEmpty = false;
     }
 
     @Override
     protected void onMethodExit(int opcode) {
-        if (opcode != Opcodes.ATHROW) {
+        if (opcode != Opcodes.ATHROW && !isEmpty) {
             mv.visitLabel(endTryLabel);
         }
     }
 
     @Override
     public void visitMaxs(int maxStack, int maxLocals) {
-        mv.visitLabel(startCatchLabel);
-        mv.visitVarInsn(Opcodes.ASTORE, 1);
-        mv.visitVarInsn(Opcodes.ALOAD, 1);
-        mv.visitInsn(Opcodes.ATHROW);
-        mv.visitTryCatchBlock(startTryLabel, endTryLabel, startCatchLabel, "java/lang/Exception");
+        if(!isEmpty) {
+            mv.visitLabel(startCatchLabel);
+            mv.visitVarInsn(Opcodes.ASTORE, 1);
+            mv.visitVarInsn(Opcodes.ALOAD, 1);
+            mv.visitInsn(Opcodes.ATHROW);
+            mv.visitTryCatchBlock(startTryLabel, endTryLabel, startCatchLabel, "java/lang/Exception");
+        }
         super.visitMaxs(maxStack, maxLocals);
     }
 }
