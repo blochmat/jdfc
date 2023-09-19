61f1ba5efb287419754e19eecca51840a3af4882
Matthias Bloch <m4bl94@gmail.com>
2023-09-19 10:44:17 +0200

    Minor changes

--- com.jdfc.core/src/test/java/graphs/sg/SGCreatorTest.java
+++ com.jdfc.core/src/test/java/graphs/sg/SGCreatorTest.java
@@ -1,4 +1,12 @@
 package graphs.sg;
 
 public class SGCreatorTest {
+
+
+    // create a cfg mock objects
+
+    // sg needs to have
+    // nodes = sum of nodes from all sub graphs
+    // edges = sum of edges from all sub graphs + 3 (call-entry, exit-return, call-return)
+
 }
